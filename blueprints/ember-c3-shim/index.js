7862a50b2a6dda7f1d794f92cd75580095cd3a1e
Khadim Fall <khadim.fall@alyne.com>
2018-08-17 14:02:29 +0200

    fixed issue

--- blueprints/ember-c3-shim/index.js
+++ blueprints/ember-c3-shim/index.js
@@ -4,7 +4,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('c3', '~0.4.10').then(function () {
+    return this.addPackageToProject('c3', '~0.4.10').then(function () {
       return this.addAddonToProject('ember-d3@0.2.0');
     }.bind(this));
   }
